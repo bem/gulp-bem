bcb1220c2843c18af8b08b639616c9b4b862d215
Alexey Litvinov <iamhoneyboy@gmail.com>
2015-02-08 15:28:30 +0300

    simple tests

---
+++ test/index.js
@@ -0,0 +1,36 @@
+describe('gulp-bemhtml', function () {
+  'use strict';
+
+  var bemhtml = require('..');
+  var expect = require('expect.js');
+  var gutil = require('gulp-util');
+
+  describe('stream', function () {
+    var stream;
+    var vinylFile;
+
+    before(function (next) {
+      stream = bemhtml();
+
+      stream.on('data', function (file) {
+        vinylFile = file;
+        next();
+      });
+
+      stream.write(new gutil.File({
+        path: 'page.bemhtml',
+        contents: new Buffer('block(\'page\')(tag()(\'h1\'), content()(\'Hello, world!\'));')
+      }));
+    });
+
+    it('changes file extension to *.bemhtml.js', function () {
+      expect(vinylFile.relative).to.be.equal('page.bemhtml.js');
+    });
+
+    it('outputs bemhtml templates compiler', function () {
+      eval(vinylFile.contents.toString());
+      var bemhtml = exports.BEMHTML;
+      expect(bemhtml.apply({block: 'page'})).to.be.equal('<h1 class="page">Hello, world!</h1>');
+    });
+  });
+});
