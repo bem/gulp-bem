e9dc09258cfbd7866ab181b11ab08bf20a69b814
Alexey Litvinov <iamhoneyboy@gmail.com>
2015-02-15 01:07:16 +0300

    Added marker to the code fragment containing error

--- error.js
+++ error.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var util = require('util');
+var format = util.format;
+
 /**
  * @param  {object} err
  * @param  {string} syntax
@@ -8,20 +11,28 @@
  */
 module.exports = function (err, syntax, code) {
   // Assume that esprima parser failed
-  if (err.description && err.lineNumber) {
-    var shownLines = 3;
-    var fSyntax = syntax.split('\n');
-    var fCode = code.split('\n');
-    var synLen = fSyntax.length;
-    var lineNumber = err.lineNumber - synLen;
+  if (err.description && err.column && err.lineNumber) {
+    var syntaxLines = syntax.split('\n').length;
 
-    if (lineNumber < 0) {
+    // Looks like we got corrupted bemhtml syntax file
+    if (syntaxLines >= err.lineNumber) {
       return 'Problems with syntax file';
     }
 
-    var start = lineNumber - shownLines - 1;
-    var fragment = fCode.slice(start > 0 ? start : 0, lineNumber + shownLines);
-    return err.description + '\n\n' + fragment.join('\n');
+    var source = code.split('\n');
+
+    var addtionalLines = 3;
+    var errorLine = err.lineNumber - syntaxLines; // extra line from length prop
+    var startLine = Math.max(errorLine - addtionalLines, 0);
+    var endLine = Math.min(errorLine + addtionalLines, source.length);
+
+    var fragment = source.slice(startLine, endLine);
+    // Adding marker
+    fragment.splice(errorLine - startLine + 1, 0, Array(err.column).join(' ') + '^');
+
+    var message = format('%s\n\n%s', err.description, fragment.join('\n'));
+
+    return message;
   }
 
   return err;
