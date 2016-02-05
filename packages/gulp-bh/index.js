9811bb1b346d30c2a0b7681f9a6223362dcde120
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2016-02-05 16:46:17 +0300

    Add filename for js errors

--- packages/gulp-bh/index.js
+++ packages/gulp-bh/index.js
@@ -8,7 +8,7 @@ module.exports = function(opt) {
     var bh = new BH();
     opt && bh.setOptions(opt);
 
-    function evalWithBH(content) {
+    function evalWithBH(content, filename) {
         var sandbox = {}
         var exports = {}
 
@@ -17,16 +17,16 @@ module.exports = function(opt) {
         sandbox.global = sandbox
         sandbox.bh = bh;
 
-        return vm.runInNewContext(content, sandbox);
+        return vm.runInNewContext(content, sandbox, {filename: filename});
     }
 
     return {
         match: function() {
             return through.obj(function(file, enc, next) {
                 var fileContent = file.contents.toString(enc);
-                matcher = evalWithBH(fileContent);
+                matcher = evalWithBH(fileContent, file.relative);
                 if (matcher !== bh) {
-                    // module.exports = function(bh) {
+                    // module.exports = function(bh)
                     matcher(bh);
                 }
                 next(null, file);
@@ -35,13 +35,14 @@ module.exports = function(opt) {
         apply: function(filePath) {
             return through.obj(function(file, enc, next) {
                 var fileContent = file.contents.toString(enc);
+                bemJson = evalWithBH(fileContent, file.relative);
                 if (filePath) {
                     file.path = path.join(file.base, filePath);
                 }
-                bemJson = evalWithBH(fileContent);
                 file.contents = new Buffer(bh.apply(bemJson));
                 next(null, file);
             });
         }
     };
+
 };
